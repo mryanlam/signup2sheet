ab36a8b1a124854be592735e3c9b15f336b9c8c9
Matthew Lam <matt@mryanlam.com>
2021-07-15 01:04:20 -0700

    discord bot portion

--- signup2sheet/signup2sheet.py
+++ signup2sheet/signup2sheet.py
@@ -1,5 +1,39 @@
-from typing import Dict
+from typing import Dict, List
 from ruamel.yaml import YAML
+from raid_helper_aggregator import raid_helper_aggregator
+import discord
+
+client = discord.Client()
+raid_helper_id = 579155972115660803
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$scrape'):
+        print("Test command called")
+        print(message.channel.id)
+        raid_ids = await _scrape_channel(message.channel)
+        raid_helper = raid_helper_aggregator(raid_ids, config["token"])
+        raid_helper.build_output()
+        await message.channel.send(raid_helper.output)
+
+
+async def _scrape_channel(channel) -> List[str]:
+    res = list()
+    async for elem in channel.history():
+        print(elem)
+        if elem.author.id == raid_helper_id:
+            print("Found raid-helper msg {}".format(elem.id))
+            res.append(str(elem.id))
+    return res
 
 
 def read_config(path: str) -> Dict[str, str]:
@@ -13,6 +47,6 @@ def read_config(path: str) -> Dict[str, str]:
         }
 
 
+config = read_config("conf.yaml")
 if __name__ == '__main__':
-    config = read_config("conf.yaml")
-    print(config)
+    client.run(config["discord_token"])
