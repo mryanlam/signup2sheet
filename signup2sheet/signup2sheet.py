3709a1d74bfc7267a17f57b6d68e4c88ec8b2533
Matthew Lam <matt@mryanlam.com>
2021-07-15 00:16:43 -0700

    read config

--- signup2sheet/signup2sheet.py
+++ signup2sheet/signup2sheet.py
@@ -1,1 +1,18 @@
-print("Hello World")
+from typing import Dict
+from ruamel.yaml import YAML
+
+
+def read_config(path: str) -> Dict[str, str]:
+    with open(path, 'r') as f:
+        yaml = YAML()
+        config = yaml.load(f)
+        return {
+            "token": config["token"],
+            "sheet": config["sheet"],
+            "discord_token": config["discord_token"]
+        }
+
+
+if __name__ == '__main__':
+    config = read_config("conf.yaml")
+    print(config)
